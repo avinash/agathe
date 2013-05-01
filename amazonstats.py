f229ffc7e61f3a0b9a54535bc3ec37826e47af64
Avinash Meetoo <avinash@knowledge7.com>
2013-05-01 11:40:02 +0400

    Follow spacing and tabbing rules (as in PEP-8) for the script.

--- amazonstats.py
+++ amazonstats.py
@@ -12,24 +12,24 @@ from amazonconfig import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AMAZON_ASSOCI
 amazon = AmazonAPI(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AMAZON_ASSOCIATE_TAG)
 
 def lookup(asin):
-	try:
-		product = amazon.lookup(ItemId=asin)
-	except AsinNotFound:
-		return str(asin) + '\tNot Found'
+    try:
+        product = amazon.lookup(ItemId=asin)
+    except AsinNotFound:
+        return str(asin) + '\tNot Found'
 
-	price = product.list_price[0]
-	sales_rank = product._safe_get_element('SalesRank')
+    price = product.list_price[0]
+    sales_rank = product._safe_get_element('SalesRank')
 
-	reviews_url = product.reviews[1]
-	soup = BeautifulSoup(urlopen(reviews_url))
+    reviews_url = product.reviews[1]
+    soup = BeautifulSoup(urlopen(reviews_url))
 
-	span = soup.find('span', { 'class': 'crAvgStars'})
-	avg_rating = span.contents[0].contents[1].contents[0]['alt'].split()[0]
-	num_ratings = span.contents[2].contents[0].split()[0]
+    span = soup.find('span', { 'class': 'crAvgStars'})
+    avg_rating = span.contents[0].contents[1].contents[0]['alt'].split()[0]
+    num_ratings = span.contents[2].contents[0].split()[0]
 
-	return str(asin) + '\t' + str(sales_rank) + '\t' + str(price) + '\t' + str(avg_rating) + '\t' + str(num_ratings)
+    return str(asin) + '\t' + str(sales_rank) + '\t' + str(price) + '\t' + str(avg_rating) + '\t' + str(num_ratings)
 
 for line in fileinput.input():
-	asin = line.rstrip('\n')
-	sys.stdout.write(lookup(asin) + '\n')
-	sys.stdout.flush()
+    asin = line.rstrip('\n')
+    sys.stdout.write(lookup(asin) + '\n')
+    sys.stdout.flush()
