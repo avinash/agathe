f94dc9e651f9cc4ace1433fbd3b0ff3518db3ef8
Avinash Meetoo <avinash@knowledge7.com>
2020-07-24 11:28:52 +0400

    The program works in principle (but Amazon does not allow me to test as I have not generated any sales...)

--- amazonstats.py
+++ amazonstats.py
@@ -5,37 +5,17 @@
 import sys
 import fileinput
 
-from amazonconfig import AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AMAZON_ASSOCIATE_TAG
-from amazon.api import AmazonAPI, AsinNotFound
-from BeautifulSoup import BeautifulSoup
+from amazonconfig import KEY, SECRET, TAG, COUNTRY
+from amazon.paapi import AmazonAPI
+from bs4 import BeautifulSoup
 from time import sleep
-from urllib2 import urlopen, HTTPError
+from urllib.request import urlopen, HTTPError
 
-def lookup(asin):
+def get_product(asin):
     """Looks up the asin in Amazon and print details about it"""
-    try:
-        product = amazon.lookup(IdType='ISBN', SearchIndex='Books', ItemId=asin)
-    except AsinNotFound:
-        return str(asin) + '\tNot Found'
+    product = amazon.get_product(asin)
 
-    if isinstance(product, list):
-        product = product[0]
-
-    price = product.list_price[0]
-    sales_rank = product._safe_get_element('SalesRank')
-
-    reviews_url = product.reviews[1]
-
-    reviews = None
-
-    while not reviews:
-        try:
-            reviews = urlopen(reviews_url)
-        except HTTPError:
-            # let us wait one second and we try again
-            sleep(1)
-
-    soup = BeautifulSoup(reviews)
+    """soup = BeautifulSoup(reviews)
     span = soup.find('span', {'class': 'crAvgStars'})
 
     if span is None:
@@ -44,14 +24,15 @@ def lookup(asin):
     avg_rating = span.contents[0].contents[1].contents[0]['alt'].split()[0]
     num_ratings = span.contents[2].contents[0].split()[0]
 
-    return str(asin) + '\t' + str(sales_rank) + '\t' + str(price) + '\t' + str(avg_rating) + '\t' + str(num_ratings)
+    return str(asin) + '\t' + str(sales_rank) + '\t' + str(price) + '\t' + str(avg_rating) + '\t' + str(num_ratings)"""
+    return product
 
 # The main program which connects to Amazon
 # and queries the API for details about a
 # list of asins
-amazon = AmazonAPI(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AMAZON_ASSOCIATE_TAG, MaxQPS=0.9)
+amazon = AmazonAPI(KEY, SECRET, TAG, COUNTRY)
 
 for line in fileinput.input():
     an_asin = line.rstrip('\n')
-    sys.stdout.write(lookup(an_asin) + '\n')
+    sys.stdout.write(get_product(an_asin) + '\n')
     sys.stdout.flush()
