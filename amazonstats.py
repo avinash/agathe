e236e611f70943284dd9bb6ce34edec4873a07ff
Avinash Meetoo <avinash@knowledge7.com>
2020-08-10 11:28:34 +0400

    Reformatted the code to be compliant with PEP8

--- amazonstats.py
+++ amazonstats.py
@@ -1,13 +1,14 @@
 from selectorlib import Extractor
 from time import sleep
 import re
-import requests 
+import requests
 import fileinput
 
 # Create an Extractor by reading from the YAML file
 e = Extractor.from_yaml_file('selectors.yml')
 
-def scrape(url):  
+
+def scrape(url):
 
     headers = {
         'dnt': '1',
@@ -24,44 +25,45 @@ def scrape(url):
 
     # Download the page using requests
     r = requests.get(url, headers=headers)
-    
+
     # Simple check to check if page was blocked (Usually 503)
     if r.status_code > 500:
         if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%asin)
+            print("Page %s was blocked by Amazon. Please try using better proxies\n" % asin)
         else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(asin,r.status_code))
+            print("Page %s must have been blocked by Amazon as the status code was %d" % (asin, r.status_code))
         return None
-    
-    # Pass the HTML of the page and create 
+
+    # Pass the HTML of the page and create
     return e.extract(r.text)
 
+
 for asin in fileinput.input():
-    data = scrape("https://www.amazon.com/dp/product/" + asin.rstrip()) 
+    data = scrape("https://www.amazon.com/dp/product/" + asin.rstrip())
 
     title = data['title']
-    
+
     author = data['author']
-    if author != None:
+    if author is not None:
         author = re.sub(r'^.*\[', '', author)
         author = re.sub(r'\].*$', '', author)
     else:
         author = ""
-    
+
     if data['sales_rank'] is None:
         sales_rank = ""
     else:
-        sales_rank = data['sales_rank'].split()[4].replace("#","").replace(",","")
-    
+        sales_rank = data['sales_rank'].split()[4].replace("#", "").replace(",", "")
+
     if data['rating'] is None:
         rating = ""
     else:
         rating = data['rating'].split()[0]
-    
+
     if data['number_of_ratings'] is None:
         number_of_ratings = ""
     else:
-        number_of_ratings = data['number_of_ratings'].split()[0].replace(",","")
+        number_of_ratings = data['number_of_ratings'].split()[0].replace(",", "")
 
     print(asin.rstrip(), title, author, sales_rank, rating, number_of_ratings, sep="\t", flush=True)
 
